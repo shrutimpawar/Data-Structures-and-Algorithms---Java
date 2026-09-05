53a6b6750f32225f80f4f92a9f2233a8caed966a
Shruti Pawar <shrutimpawar2526@gmail.com>
2026-09-05 21:32:20 +0530

    Rotate Array Elements

---
+++ Arrays/RotateArray.java
@@ -0,0 +1,49 @@
+package Arrays;
+
+import java.util.*;
+
+public class RotateArray{
+
+    public static void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+        k = k % n;
+
+        int[] temp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = temp[i];
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+        rotate(nums, k);
+
+        System.out.println("Rotated array:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}
