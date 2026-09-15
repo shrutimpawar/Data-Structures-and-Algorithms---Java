556587c674a6a375a61ad42547d6deb74d446141
Shruti Pawar <shrutimpawar2526@gmail.com>
2026-09-15 21:04:07 +0530

    Valid Parentheses

---
+++ Arrays/ValidParentheses.java
@@ -0,0 +1,57 @@
+package Stack;
+
+import java.util.*;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            // Opening brackets
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+
+            // Closing brackets
+            else {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter brackets: ");
+        String s = sc.nextLine();
+
+        boolean result = isValid(s);
+
+        System.out.println("Valid Parentheses = " + result);
+
+        sc.close();
+    }
+}
