b2741585e381fd3a989eba4b63e68bb6f72911ef
Shruti Pawar <shrutimpawar2526@gmail.com>
2026-09-17 22:12:30 +0530

    Valid palindrome of strings

---
+++ Arrays/ValidPalindrome.java
@@ -0,0 +1,51 @@
+
+package Arrays;
+
+import java.util.*;
+
+public class ValidPalindrome {
+
+    public static boolean isPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            // Skip non-alphanumeric characters
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // Compare characters
+            if (Character.toLowerCase(s.charAt(left))
+                    != Character.toLowerCase(s.charAt(right))) {
+
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        boolean result = isPalindrome(s);
+
+        System.out.println("Is Palindrome: " + result);
+
+        sc.close();
+    }
+}
