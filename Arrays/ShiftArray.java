96b8a87d3743b429683537c160f80482ffcdad7b
Shruti Pawar <shrutimpawar2526@gmail.com>
2026-09-23 23:22:31 +0530

    Shift array by k

---
+++ Arrays/ShiftArray.java
@@ -0,0 +1,45 @@
+package Arrays;
+import java.util.Scanner;
+
+public class ShiftArray {
+
+    static void shiftByK(int[] arr, int k) {
+
+        for (int j = 1; j <= k; j++) {
+
+            int last = arr[arr.length - 1];
+
+            for (int i = arr.length - 1; i > 0; i--) {
+                arr[i] = arr[i - 1];
+            }
+
+            arr[0] = last;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter K: ");
+        int k = sc.nextInt();
+
+        shiftByK(arr, k);
+
+        sc.close();
+    }
+}
